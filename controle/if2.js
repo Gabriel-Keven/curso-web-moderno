701b81bdb8170a492d7f2158811dcb5b2e4fd35e
Gabriel-Keven <78623647+Gabriel-Keven@users.noreply.github.com>
2022-03-01 16:18:43 -0300

    Add files via upload

---
+++ controle/if2.js
@@ -0,0 +1,17 @@
+function teste1(num){
+    if(num>7) 
+        console.log(num);
+        console.log('Final');
+}
+teste1(6);
+teste1(8);
+console.log('------------------------------------------\n');
+function teste2(num){
+    if(num>7)
+        ;//Senteça de código vázia associada ao if -> Cuidado com o ';', não usar com as estruturas de controle
+    {
+        console.log(num);
+    }
+}
+teste2(6);
+teste2(8);
